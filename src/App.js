209ed24af232bfc6f07a8de71280939e6d5895e9
Jubin John <72947744+jubjohn@users.noreply.github.com>
2020-10-15 21:57:21 -0400

    Changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Docker compose works!
+          I was changed on the feature branch :P
         </p>
         <a
           className="App-link"
